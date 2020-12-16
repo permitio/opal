163106314bc7e7d7824bb6f86902a8942e59bd5e
Asaf Cohen <asafc@me.com>
2020-12-16 11:50:24 +0200

    add missing api routes

--- horizon/main.py
+++ horizon/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
-from horizon.server.api import router as api_router
+from horizon.server.api import router as proxy_router
+from horizon.enforcer.api import router as enforcer_router
+from horizon.policy.api import router as policy_router
 from horizon.server.middleware import configure_middleware
 from horizon.logger import logger
 from horizon.policy.updater import policy_updater
@@ -10,7 +12,9 @@ app = FastAPI(title="Horizon Sidecar", version="0.1.0")
 configure_middleware(app)
 
 # include the api routes
-app.include_router(api_router)
+app.include_router(proxy_router)
+app.include_router(enforcer_router)
+app.include_router(policy_router)
 
 @app.get("/healthcheck", include_in_schema=False)
 @app.get("/", include_in_schema=False)
