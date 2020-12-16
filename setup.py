6b19169c98fc82942b8aa36533162bf3d77b8d12
Asaf Cohen <asafc@me.com>
2020-12-16 16:39:56 +0200

    dockerize sidecar

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_requirements(env=""):
         return [x.strip() for x in fp.read().split("\n") if not x.startswith("#")]
 
 setup(
-    name='acalla',
+    name='horizon',
     version='0.1.0',
     packages=find_packages(),
     python_requires='>=3.8',
