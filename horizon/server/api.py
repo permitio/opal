aad29e6366034830cea6bbdb30478ff8584c2f26
Asaf Cohen <asafc@me.com>
2021-02-07 18:08:16 +0200

    bugfix in cloud api proxy: did not raise 401 if no token is provided

--- horizon/server/api.py
+++ horizon/server/api.py
@@ -29,7 +29,7 @@ async def sdk_proxy(path: str, request: Request):
     """
     auth_header = request.headers.get("Authorization")
     if auth_header is None:
-        HTTPException(
+        raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Must provide a bearer token!",
             headers={"WWW-Authenticate": "Bearer"}
