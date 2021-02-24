3ed6325fd89384d4eb316b493630e67e91bc4e7e
orweis <orweis@gmail.com>
2021-02-24 23:17:02 +0200

    some docs

--- opal/fetcher/fetching_engine.py
+++ opal/fetcher/fetching_engine.py
@@ -1,22 +1,38 @@
 import asyncio
 from asyncio import events
 from typing import Coroutine
-from .events import FetchEvent
+import typing
+from .events import FetchEvent, FetcherConfig
 from .fetcher_arsenal import FetcherArsenal
 from .data_fetcher import BaseFetchProvider
+from .logger import get_logger
+
+logger = get_logger("engine")
 
 
 async def fetch_worker(queue:asyncio.Queue, arsenal:FetcherArsenal):
+    """
+    The worker task performing items added to the Engine's Queue
+
+    Args:
+        queue (asyncio.Queue): [description]
+        arsenal (FetcherArsenal): [description]
+    """
 
     while True:
         event: FetchEvent
         callback: Coroutine
         event, callback = await queue.get()
         try:
-            fetcher = arsenal.get_fetcher_for_event(event)
+            fetcher = arsenal.get_fetcher_for_event(event)            
             assert fetcher is not None
             data = await fetcher.fetch()
-            await callback(data)
+            try:
+                await callback(data)
+            except: 
+                logger.exception(f"Callback - {callback} failed")
+        except:
+            logger.exception("failed to process fetch event")
         finally:
             # Notify the queue that the "work item" has been processed.
             queue.task_done()
@@ -24,9 +40,10 @@ async def fetch_worker(queue:asyncio.Queue, arsenal:FetcherArsenal):
 
 
 class FetchingEngine:
+
+    DEFAULT_WORKER_COUNT=5
     
-    
-    def __init__(self, arsenal_config=None, worker_count=4) -> None:
+    def __init__(self, arsenal_config=None, worker_count=DEFAULT_WORKER_COUNT) -> None:
         self._queue = asyncio.Queue()
         self._tasks = []
         self._fetcher_arsenal = FetcherArsenal(arsenal_config)
@@ -34,14 +51,45 @@ class FetchingEngine:
         for _ in range(worker_count):
             self.create_worker()
 
+    async def __aenter__(self):
+        """
+        Async Context manager to cancel tasks on exit 
+        """
+        return self
 
-    def handle_url(self, url, callback:Coroutine):
-        event = FetchEvent(url=url, fetcher="HttpGetFetchProvider")
-        return self.handle_fetch_event(event, callback)
+    async def  __aexit__(self, exc_type, exc, tb):
+        if (exc is not None):
+            logger.error("Error occurred within FetchingEngine context", exc_info=(exc_type, exc, tb))
+        # Cancel our worker tasks.
+        for task in self._tasks:
+            task.cancel()
+        # Wait until all worker tasks are cancelled.
+        await asyncio.gather(*self._tasks, return_exceptions=True)
 
-    def handle_fetch_event(self, event:FetchEvent, callback:Coroutine):
+
+    async def queue_url(self, url:str, callback:Coroutine, fetcher_config:FetcherConfig=None, fetcher="HttpGetFetchProvider"):
+        """
+        Simplified default fetching handler for queuing a fetch task
+
+        Args:
+            url (str): the URL to fetch from
+            callback (Coroutine): a callback to call with the fetched result
+            fetcher_config (FetcherConfig, optional): Configuration to be used by the fetcher. Defaults to None.
+            fetcher (str, optional): Which fetcher class to use. Defaults to "HttpGetFetchProvider".
+        """
+        #init a URL event
+        event = FetchEvent(url=url, fetcher=fetcher, fetcher_config=fetcher_config)
+        await self.queue_fetch_event(event, callback)
+
+    async def queue_fetch_event(self, event:FetchEvent, callback:Coroutine):
+        """Basic handler to queue a fetch event for a fetcher class
+
+        Args:
+            event (FetchEvent): the fetch event to queue as a task
+            callback (Coroutine): a callback to call with the fetched result
+        """
         # add to the queue for handling
-        self._queue.put_nowait((event, callback))
+        await self._queue.put((event, callback))
 
 
     def create_worker(self)-> asyncio.Task:
