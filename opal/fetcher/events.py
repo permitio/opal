489035180430bfee0d4531b91a07f83529bdba88
orweis <orweis@gmail.com>
2021-02-25 08:35:54 +0200

    cleanup

--- opal/fetcher/events.py
+++ opal/fetcher/events.py
@@ -9,16 +9,16 @@ class FetcherConfig(BaseModel):
     """
     pass
 
+
 class FetchEvent(BaseModel):
     """
     Event used to describe an queue fetching tasks
     """
     # optional name of the specific event
-    name:str = ""
+    name: str = ""
     # A string identifying the fetcher class to use (as registered in the fetcher arsenal)
-    fetcher:str
+    fetcher: str
     # The url the event targets for fetching
     url: str
     #  Specific fetcher configuration (overridden by deriving event classes)
     config: FetcherConfig = None
-
