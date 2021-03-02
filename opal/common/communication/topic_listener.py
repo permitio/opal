5915bae89825bcf366432c344723d916530c5d7a
Asaf Cohen <asafc@me.com>
2021-03-02 19:00:33 +0200

    topic listener small refactor

--- opal/common/communication/topic_listener.py
+++ opal/common/communication/topic_listener.py
@@ -14,36 +14,44 @@ class TopicCallback(Protocol):
 
 class TopicListenerThread:
     """
-    Publishes changes made to policy (rego) via WS server, triggered by PolicyWatcher.
+    Runs a PubSubClient in a separate thread, listens to a predefined
+    topic list and runs a callback when messages arrive for that topic.
     """
     def __init__(
         self,
+        client: PubSubClient,
         server_uri: str,
         topics: TopicList = None,
         callback: TopicCallback = None,
-        extra_headers: Optional[List[Tuple[str, str]]] = None,
     ):
         self._thread = AsyncioEventLoopThread(name="TopicListenerThread")
+        self._client = client
         self._server_uri = server_uri
         self._topics = topics
         self._callback = callback
-        self._extra_headers = extra_headers
 
     def start(self):
+        """
+        starts the listener thread and run a pubsub client in the thread's event loop.
+        the client will attempt to connect to the pubsub server until successful.
+        """
         logger.info("started topic listener", topics=self._topics)
         self._thread.create_task(self._run_client())
         self._thread.start()
 
-    async def _run_client(self):
-        self._client = PubSubClient(
-            extra_headers=self._extra_headers,
-        )
-        for topic in self._topics:
-            self._client.subscribe(topic, self._callback)
-        self._client.start_client(f"{self._server_uri}", loop=self._thread.loop)
-
     async def stop(self):
+        """
+        stops the pubsub client, and then stops the thread.
+        """
         await self._client.disconnect()
         self._thread.stop()
         logger.info("stopped topic listener", topics=self._topics)
+
+    async def _run_client(self):
+        """
+        starts the client in the thread's event loop
+        """
+        for topic in self._topics:
+            self._client.subscribe(topic, self._callback)
+        self._client.start_client(f"{self._server_uri}", loop=self._thread.loop)
 
--- opal/server/policy/github_webhook/listener.py
+++ opal/server/policy/github_webhook/listener.py
@@ -8,17 +8,17 @@ The solution is quite simply: the worker that serves the request simply
 publishes on the `webhook` topic, and the listener's callback is triggered.
 """
 
+from fastapi_websocket_pubsub.pub_sub_client import PubSubClient
 from opal.common.utils import get_authorization_header
 from opal.common.communication.topic_listener import TopicListenerThread
 from opal.server.policy.watcher import trigger_webhook
-from opal.server.config import (
-    OPAL_WS_LOCAL_URL,
-    OPAL_WS_TOKEN
-)
+from opal.server.config import OPAL_WS_LOCAL_URL, OPAL_WS_TOKEN
 
 webhook_listener = TopicListenerThread(
+    client=PubSubClient(
+        extra_headers=[get_authorization_header(OPAL_WS_TOKEN)]
+    ),
     server_uri=OPAL_WS_LOCAL_URL,
     topics=["webhook"],
     callback=trigger_webhook,
-    extra_headers=[get_authorization_header(OPAL_WS_TOKEN)]
 )
