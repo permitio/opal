d05d5b8341bce31e7d20f20c8cade0960b67f167
orweis <orweis@gmail.com>
2021-03-03 03:57:02 +0200

    test and fixes for test

--- opal/client/data/updater.py
+++ opal/client/data/updater.py
@@ -1,5 +1,6 @@
+from opal.client.policy_store.base_policy_store_client import BasePolicyStoreClient
 from opal.fetcher.events import FetcherConfig
-from typing import Dict, List
+from typing import Dict, Iterable, List
 from opal.common.schemas.data import DataUpdate, DataUpdateEntry
 from fastapi_websocket_rpc.rpc_channel import RpcChannel
 from fastapi_websocket_pubsub import PubSubClient
@@ -8,7 +9,7 @@ from opal.client.logger import get_logger
 from opal.client.config import DATA_TOPICS, DATA_UPDATES_WS_URL, CLIENT_TOKEN, KEEP_ALIVE_INTERVAL
 from opal.client.utils import AsyncioEventLoopThread, get_authorization_header
 from opal.client.policy_store.policy_store_client_factory import DEFAULT_POLICY_STORE
-from opal.client.data.fetcher import data_fetcher
+from opal.client.data.fetcher import DataFetcher
 from opal.client.data.rpc import TenantAwareRpcEventClientMethods
 
 
@@ -16,18 +17,20 @@ logger = get_logger("Opal Client")
 updater_logger = get_logger("Data Updater")
 
 
-async def update_policy_data(update:DataUpdate=None, policy_store=DEFAULT_POLICY_STORE):
+async def update_policy_data(update: DataUpdate = None, policy_store: BasePolicyStoreClient = DEFAULT_POLICY_STORE, data_fetcher=None):
     """
     fetches policy data (policy configuration) from backend and updates it into policy-store (i.e. OPA)
     """
+    if data_fetcher is None:
+        data_fetcher = DataFetcher()
     # types
     urls: Dict[str, FetcherConfig] = None
     url_to_entry: Dict[str, DataUpdateEntry] = None
-    # if we have an actual specification for the update 
+    # if we have an actual specification for the update
     if update is not None:
         entries: List[DataUpdateEntry] = update.entries
-        urls = {entry.url : entry.config for entry in entries}
-        url_to_entry = {entry.url : entry for entry in entries}
+        urls = {entry.url: entry.config for entry in entries}
+        url_to_entry = {entry.url: entry for entry in entries}
     # get the data for the update
     updater_logger.info("Fetching policy data", urls=urls.keys())
     policy_data_by_urls = await data_fetcher.fetch_policy_data(urls)
@@ -36,7 +39,7 @@ async def update_policy_data(update:DataUpdate=None, policy_store=DEFAULT_POLICY
     for url in policy_data_by_urls:
         # get path to store the URL data (default mode (None) is as "" - i.e. as all the data at root)
         entry = url_to_entry.get(url, None)
-        policy_store_path = "" if entry is None  else entry.dst_path
+        policy_store_path = "" if entry is None else entry.dst_path
         # None is not valid - use "" (protect from missconfig)
         if policy_store_path is None:
             policy_store_path = ""
@@ -44,22 +47,31 @@ async def update_policy_data(update:DataUpdate=None, policy_store=DEFAULT_POLICY
         if policy_store_path != "" and not policy_store_path.startswith("/"):
             policy_store_path = f"/{policy_store_path}"
         policy_data = policy_data_by_urls[url]
-        policy_store.set_policy_data(policy_data, path=policy_store_path)
+        await policy_store.set_policy_data(policy_data, path=policy_store_path)
 
 
-async def refetch_policy_data_and_update_store(policy_store=DEFAULT_POLICY_STORE):
+async def refetch_policy_data_and_update_store(policy_store: BasePolicyStoreClient = DEFAULT_POLICY_STORE):
     """
     will bring fresh data from backend, and will inject into OPA.
     """
     # TODO - this should be replaced by an update coming the server on client connect
     await update_policy_data(policy_store=policy_store)
 
+
 class DataUpdater:
-    def __init__(self, token=CLIENT_TOKEN, server_url=DATA_UPDATES_WS_URL, data_topics=None, policy_store=DEFAULT_POLICY_STORE):
+    def __init__(self, token: str = CLIENT_TOKEN, 
+                 server_url: str = DATA_UPDATES_WS_URL,
+                 fetch_on_connect:bool=True,
+                 data_topics: List[str] = None, 
+                 policy_store: BasePolicyStoreClient = DEFAULT_POLICY_STORE):
+        # Should the client use the default data source to fetch on connect
+        self._fetch_on_connect = fetch_on_connect
+        # The policy store we'll save data updates into
         self._policy_store = policy_store
-        self._data_topics = DATA_TOPICS
-        if data_topics is not None:
-            self._data_topics.extend(data_topics)
+        # Pub/Sub topics we subscribe to for data updates
+        self._data_topics = data_topics if data_topics is not None else DATA_TOPICS
+        # Data fetcher
+        self._data_fetcher = DataFetcher()
         self._thread = AsyncioEventLoopThread(name="PolicyUpdaterThread")
         self._token = token
         self._server_url = server_url
@@ -68,26 +80,27 @@ class DataUpdater:
         else:
             self._extra_headers = [get_authorization_header(self._token)]
 
-    async def _update_policy_data(self, update:DataUpdate=None):
+    async def _update_policy_data(self, data: dict = None, topic=""):
         """
         will run when we get notifications on the policy_data topic.
         i.e: when new roles are added, changes to permissions, etc.
         """
-        if update is not None:
-            reason = update.reason
+        if data is not None:
+            reason = data.get("reason", "")
         else:
             reason = "Periodic update"
         updater_logger.info("Updating policy data", reason=reason)
-
+        update = DataUpdate.parse_obj(data)
         await update_policy_data(update, policy_store=self._policy_store)
 
-    async def on_connect(self, client:PubSubClient, channel:RpcChannel):
+    async def on_connect(self, client: PubSubClient, channel: RpcChannel):
         # on connection to backend, whether its the first connection
         # or reconnecting after downtime, refetch the state opa needs.
         updater_logger.info("Connected to server")
-        await refetch_policy_data_and_update_store(policy_store=self._policy_store)
+        if self._fetch_on_connect:
+            await refetch_policy_data_and_update_store(policy_store=self._policy_store)
 
-    async def on_disconnect(self, channel:RpcChannel):
+    async def on_disconnect(self, channel: RpcChannel):
         updater_logger.info("Disconnected from server")
 
     def start(self):
@@ -97,25 +110,22 @@ class DataUpdater:
 
     async def _run_client(self):
         logger.info("Launching data updater")
+        updater_logger.info("Subscribing to topics", topics=self._data_topics)
         self._client = PubSubClient(
-            self._token,
+            self._data_topics,
+            self._update_policy_data,
             methods_class=TenantAwareRpcEventClientMethods,
             on_connect=[self.on_connect],
             extra_headers=self._extra_headers,
-            keep_alive=KEEP_ALIVE_INTERVAL
+            keep_alive=KEEP_ALIVE_INTERVAL,
+            server_uri=self._server_url
         )
-        self._subscribe_for_updates()
-        self._client.start_client(f"{self._server_url}", loop=self._thread.loop)
-
-    def _subscribe_for_updates(self):
-        updater_logger.info("Subscribing to topics", topics=self._data_topics)
-        for topic in self._data_topics:
-            self._client.subscribe(topic, self._update_policy_data)
+        async with self._client:
+            await self._client.wait_until_done()
 
     async def stop(self):
         logger.info("Stopping data updater")
         await self._client.disconnect()
-        # stop the data fetcher singleton 
-        await data_fetcher.stop()
+        # stop the data fetcher
+        await self._data_fetcher.stop()
         self._thread.stop()
-
--- opal/client/tests/data_updater_test.py
+++ opal/client/tests/data_updater_test.py
@@ -1,23 +1,87 @@
+
 import os
 import sys
-
+import logging
 
 # Add parent path to use local src as package for tests
 root_dir = os.path.abspath(os.path.join(os.path.basename(__file__), os.path.pardir))
 sys.path.append(root_dir)
 
 import asyncio
+from multiprocessing import Process, Event
+from aiohttp_requests import requests
 import pytest
 import uvicorn
+from fastapi_websocket_pubsub import PubSubClient
 
+PORT = int(os.environ.get("PORT") or "9123")
+UPDATES_URL = f"ws://localhost:{PORT}/ws"
+DATA_ROUTE = "/fetchable_data"
+TRIGGER_ROUTE = "/fetchable_data"
+DATA_URL = f"http://localhost:{PORT}{DATA_ROUTE}"
+TRIGGER_URL = f"http://localhost:{PORT}{TRIGGER_ROUTE}"
+DATA_TOPICS = ["policy_data"]
+TEST_DATA = {
+    "hello": "world"
+}
 
-from opal.client.data.updater import DataUpdater
-from opal.client.policy_store.policy_store_client_factory import PolicyStoreTypes, PolicyStoreClientFactory
+from opal.client import config
+from opal.client.data.updater import DataUpdater, DataUpdate, DataUpdateEntry
+from opal.client.policy_store import PolicyStoreTypes, PolicyStoreClientFactory, MockPolicyStoreClient
+from opal.server.main import app as server_app
+from opal.server.pubsub.websocket import endpoint
+from opal.client.utils import get_authorization_header
+from opal.client.data.rpc import TenantAwareRpcEventClientMethods
 
+def setup_server(event):
+    # add a url to fetch data from  
+    @server_app.get(DATA_ROUTE)
+    def fetchable_data():
+        return TEST_DATA
+
+    @server_app.post(TRIGGER_ROUTE)
+    async def trigger_data_update(update:DataUpdate):
+        asyncio.create_task(endpoint.publish(DATA_TOPICS, data=update))
+        logging.info("published")
+
+    @server_app.on_event("startup")
+    async def startup_event():
+        #signal the server is ready
+        event.set()
+        
+    uvicorn.run(server_app, port=PORT)
+
+@pytest.fixture(scope="module")
+def server():
+    event = Event()
+    # Run the server as a separate process
+    proc = Process(target=setup_server, args=(event,), daemon=True)
+    proc.start()
+    yield event
+    proc.kill() # Cleanup after test
 
 @pytest.mark.asyncio
 async def test_client_data_updates(server):
+    server.wait(5)
     # config to use mock OPA
-    policy_store = PolicyStoreClientFactory.create()
-    updater = DataUpdater()
+    policy_store = PolicyStoreClientFactory.create(store_type=PolicyStoreTypes.MOCK)
+    updater = DataUpdater(server_url=UPDATES_URL, policy_store=policy_store, fetch_on_connect=False, data_topics=DATA_TOPICS)
+    # start the updater
+    updater.start()
+
+    # trigger an update
+    entries= [DataUpdateEntry(url=DATA_URL)]
+    update = DataUpdate(reason="Test", entries=entries)
+    async with PubSubClient(
+        server_uri=UPDATES_URL,
+        methods_class=TenantAwareRpcEventClientMethods,
+        extra_headers=[get_authorization_header(config.CLIENT_TOKEN)]
+    ) as client:
+        # Channel must be ready before we can publish on it
+        await asyncio.wait_for(client.wait_until_ready(),5)
+        await client.publish(DATA_TOPICS,data=update)
+
+    # wait until new data arrives into the strore via the updater
+    await asyncio.wait_for(policy_store.wait_for_data(),25)
+
     
