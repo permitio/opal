3e7a0cc3ea55b7f70d7e313fcb19fb20960edebd
orweis <orweis@gmail.com>
2021-03-03 03:56:10 +0200

    fix issues

--- opal/common/confi.py
+++ opal/common/confi.py
@@ -10,21 +10,28 @@ class Confi:
     def __init__(self) -> None:
         pass
 
-    def str(key, default=undefined, **kwargs)->str:
+    def str(self, key, default=undefined, **kwargs)->str:
         return config(key, default=default, **kwargs)
 
-    def int(key, default=undefined, **kwargs)->int:
+    def int(self, key, default=undefined, **kwargs)->int:
         return config(key, default=default, cast=int, **kwargs)
 
-    def float(key, default=undefined, **kwargs)->float:
+    def float(self, key, default=undefined, **kwargs)->float:
         return config(key, default=default, cast=float, **kwargs)
 
-    def list(key, default=undefined, sub_cast=text_type, delimiter=",", strip=string.whitespace, **kwargs)->list:
-        return config(key, default=default, cast=Csv(cast=sub_cast,delimiter=delimiter, strip=strip) **kwargs)
+    def list(self, key, default=undefined, sub_cast=text_type, delimiter=",", strip=string.whitespace, **kwargs)->list:
+        try:
+            return config(key, cast=Csv(cast=sub_cast,delimiter=delimiter, strip=strip), **kwargs)
+        except:
+            if default is undefined:
+                raise
+            return default
 
-    def enum(key, enum_type:T, default=undefined, **kwargs)->T:
+    def enum(self, key, enum_type:T, default=undefined, **kwargs)->T:
         try:
             return config(key, default=default, cast=enum_type, **kwargs) 
         except:
+            if default is undefined:
+                raise
             return default
     
