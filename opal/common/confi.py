c2775ebf84abadefacd66cfb67ac4c769c52ab2b
orweis <orweis@gmail.com>
2021-03-05 16:21:37 +0200

    Update config to use Confi

--- opal/common/confi.py
+++ opal/common/confi.py
@@ -1,37 +1,58 @@
-from typing import TypeVar
-from decouple import config, Csv, text_type, undefined
+from typing import TypeVar, cast
+from decouple import config, Csv, text_type, undefined, UndefinedValueError
 import string
+
+def cast_boolean(value):
+    if isinstance(value, bool):
+        return value
+    elif isinstance(value, str):        
+        value = value.lower()
+        if (value == "true"):
+            return True
+        elif value == "false":
+            return False
+        else:
+            raise UndefinedValueError(f"{value} - is not a valid boolean")
+    else:
+        raise UndefinedValueError(f"{value} - is not a valid boolean")
+
 
 
 T = TypeVar("T")
+
 
 class Confi:
 
     def __init__(self) -> None:
         pass
 
-    def str(self, key, default=undefined, **kwargs)->str:
+    def str(self, key, default=undefined, **kwargs) -> str:
         return config(key, default=default, **kwargs)
 
-    def int(self, key, default=undefined, **kwargs)->int:
+    def int(self, key, default=undefined, **kwargs) -> int:
         return config(key, default=default, cast=int, **kwargs)
 
-    def float(self, key, default=undefined, **kwargs)->float:
+    def bool(self, key, default=undefined, **kwargs) -> bool:
+        return config(key, default=default, cast=cast_boolean, **kwargs)
+
+    def float(self, key, default=undefined, **kwargs) -> float:
         return config(key, default=default, cast=float, **kwargs)
 
-    def list(self, key, default=undefined, sub_cast=text_type, delimiter=",", strip=string.whitespace, **kwargs)->list:
+    def list(self, key, default=undefined, sub_cast=text_type, delimiter=",", strip=string.whitespace, **kwargs) -> list:
         try:
-            return config(key, cast=Csv(cast=sub_cast,delimiter=delimiter, strip=strip), **kwargs)
+            return config(key, cast=Csv(cast=sub_cast, delimiter=delimiter, strip=strip), **kwargs)
         except:
             if default is undefined:
                 raise
             return default
 
-    def enum(self, key, enum_type:T, default=undefined, **kwargs)->T:
+    def enum(self, key, enum_type: T, default=undefined, **kwargs) -> T:
         try:
-            return config(key, default=default, cast=enum_type, **kwargs) 
+            return config(key, default=default, cast=enum_type, **kwargs)
         except:
             if default is undefined:
                 raise
             return default
-    
+
+# default parser
+confi = Confi()
--- opal/server/config.py
+++ opal/server/config.py
@@ -1,25 +1,26 @@
-import os
+from opal.common.confi import Confi
+
+
+confi = Confi()
 
 # ws server (TODO: merge with opal client config)
-OPAL_WS_LOCAL_URL = os.environ.get("OPAL_WS_LOCAL_URL", "ws://localhost:7002/ws")
-OPAL_WS_TOKEN = os.environ.get("OPAL_WS_TOKEN", "THIS_IS_A_DEV_SECRET")
-BROADCAST_URI = "postgres://localhost/acalladb"
+OPAL_WS_LOCAL_URL = confi.str("OPAL_WS_LOCAL_URL", "ws://localhost:7002/ws")
+OPAL_WS_TOKEN = confi.str("OPAL_WS_TOKEN", "THIS_IS_A_DEV_SECRET")
+BROADCAST_URI = confi.str("BROADCAST_URI", "postgres://localhost/acalladb")
 
 # repo watcher
-POLICY_REPO_URL = os.environ.get("POLICY_REPO_URL", None)
-POLICY_REPO_CLONE_PATH = os.environ.get("POLICY_REPO_CLONE_PATH", "regoclone")
-POLICY_REPO_MAIN_BRANCH = os.environ.get("POLICY_REPO_MAIN_BRANCH", "master")
-POLICY_REPO_MAIN_REMOTE = os.environ.get("POLICY_REPO_MAIN_REMOTE", "origin")
+POLICY_REPO_URL = confi.str("POLICY_REPO_URL", None)
+POLICY_REPO_CLONE_PATH = confi.str("POLICY_REPO_CLONE_PATH", "regoclone")
+POLICY_REPO_MAIN_BRANCH = confi.str("POLICY_REPO_MAIN_BRANCH", "master")
+POLICY_REPO_MAIN_REMOTE = confi.str("POLICY_REPO_MAIN_REMOTE", "origin")
 
 # github webhook
-POLICY_REPO_WEBHOOK_SECRET = os.environ.get("POLICY_REPO_WEBHOOK_SECRET", None)
+POLICY_REPO_WEBHOOK_SECRET = confi.str("POLICY_REPO_WEBHOOK_SECRET", None)
 
-try:
-    POLICY_REPO_POLLING_INTERVAL = int(os.environ.get("POLICY_REPO_POLLING_INTERVAL", "0"))
-except ValueError:
-    POLICY_REPO_POLLING_INTERVAL = 0
+POLICY_REPO_POLLING_INTERVAL = confi.int("POLICY_REPO_POLLING_INTERVAL", 0)
 
-ALLOWED_ORIGINS = ["*"]
+
+ALLOWED_ORIGINS = confi.list("ALLOWED_ORIGINS", ["*"])
 OPA_FILE_EXTENSIONS = ('.rego', '.json')
 
-NO_RPC_LOGS = bool(int(os.environ.get("NO_RPC_LOGS", 0)))
+NO_RPC_LOGS = confi.bool("NO_RPC_LOGS", True)
