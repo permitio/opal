924eceb47f65d60b5d3e6a82e87696ae918c83de
orweis <orweis@gmail.com>
2021-03-07 15:31:40 +0200

    organzie imports add Data publisher

--- opal/server/main.py
+++ opal/server/main.py
@@ -1,27 +1,29 @@
-import logging
-from opal.server.policy.watcher.watcher_thread import RepoWatcherThread
-from opal.common.communication.topic_publisher import TopicPublisherThread
-from fastapi_websocket_rpc.logger import logging_config, LoggingModes
-
 import asyncio
-from fastapi import FastAPI, Depends
+import logging
 from functools import partial
+from opal.server.data.data_update_publisher import DataUpdatePublisher
 from typing import Optional
 
+from fastapi import Depends, FastAPI
+from fastapi_websocket_rpc.logger import LoggingModes, logging_config
+
 from opal.common.communication.topic_listener import TopicListenerThread
+from opal.common.communication.topic_publisher import TopicPublisherThread
+from opal.common.election.pubsub_bully import PubSubBullyLeaderElection
+from opal.common.logger import get_logger
 from opal.common.middleware import configure_middleware
 from opal.common.utils import get_authorization_header
-from opal.common.logger import get_logger
-from opal.common.election.pubsub_bully import PubSubBullyLeaderElection
-
-from opal.server.deps.authentication import verify_logged_in
-from opal.server.policy.github_webhook.api import router as webhook_router
-from opal.server.policy.bundles.api import router as bundles_router
-from opal.server.policy.watcher import setup_watcher_thread, trigger_repo_watcher_pull
-from opal.server.publisher import setup_publisher_thread
-from opal.server.policy.github_webhook.listener import setup_webhook_listener
-from opal.server.pubsub import router as websocket_router
 from opal.server.config import NO_RPC_LOGS, OPAL_WS_LOCAL_URL, OPAL_WS_TOKEN
+from opal.server.data.api import init_data_updates_router
+from opal.server.deps.authentication import verify_logged_in
+from opal.server.policy.bundles.api import router as bundles_router
+from opal.server.policy.github_webhook.api import router as webhook_router
+from opal.server.policy.github_webhook.listener import setup_webhook_listener
+from opal.server.policy.watcher import (setup_watcher_thread,
+                                        trigger_repo_watcher_pull)
+from opal.server.policy.watcher.watcher_thread import RepoWatcherThread
+from opal.server.publisher import setup_publisher_thread
+from opal.server.pubsub import router as websocket_router
 
 if NO_RPC_LOGS:
     logging_config.set_mode(LoggingModes.UVICORN, level=logging.WARN)
@@ -32,6 +34,7 @@ def create_app(init_git_watcher=True, init_publisher=True) -> FastAPI:
     elected_as_leader = False
     webhook_listener: Optional[TopicListenerThread] = None
     publisher: Optional[TopicPublisherThread] = None
+    data_update_publisher: Optional[DataUpdatePublisher] = None
     watcher: Optional[RepoWatcherThread] = None
 
     app = FastAPI(
@@ -40,8 +43,16 @@ def create_app(init_git_watcher=True, init_publisher=True) -> FastAPI:
     )
     configure_middleware(app)
 
+    if init_publisher:
+        publisher = setup_publisher_thread()
+        data_update_publisher = DataUpdatePublisher(publisher)
+
+    # Init routers
+    data_updates_router = init_data_updates_router(data_update_publisher)
+
     # include the api routes
     app.include_router(bundles_router, tags=["Bundle Server"], dependencies=[Depends(verify_logged_in)])
+    app.include_router(data_updates_router, tags=["Data Updates"], dependencies=[Depends(verify_logged_in)])
     app.include_router(webhook_router, tags=["Github Webhook"])
     app.include_router(websocket_router, tags=["Pub/Sub"])
 
@@ -53,19 +64,17 @@ def create_app(init_git_watcher=True, init_publisher=True) -> FastAPI:
     async def on_election_decision(
         decision: bool,
         webhook_listener: TopicListenerThread,
-        publisher: TopicPublisherThread,
         repo_watcher: RepoWatcherThread,
     ):
         elected_as_leader = decision
         if elected_as_leader:
-            publisher.start()
             webhook_listener.start()
             repo_watcher.start()
 
     @app.on_event("startup")
     async def startup_event():
         if init_publisher:
-            publisher = setup_publisher_thread()
+            publisher.start()
             if init_git_watcher:
                 watcher = setup_watcher_thread(publisher)
                 webhook_listener = setup_webhook_listener(partial(trigger_repo_watcher_pull, watcher))
@@ -77,7 +86,6 @@ def create_app(init_git_watcher=True, init_publisher=True) -> FastAPI:
                     partial(
                         on_election_decision,
                         webhook_listener=webhook_listener,
-                        publisher=publisher,
                         repo_watcher=watcher,
                     )
                 )                
