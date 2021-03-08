cb8796aa2863fffc0710b1a6155418d5f0ceaeca
orweis <orweis@gmail.com>
2021-03-08 11:48:42 +0200

    make server init like client

--- opal/server/pubsub.py
+++ opal/server/pubsub.py
@@ -7,19 +7,29 @@ from opal.server.deps.authentication import logged_in
 
 logger = get_logger("Pub/Sub Server")
 
-router = APIRouter()
-endpoint = PubSubEndpoint(broadcaster=BROADCAST_URI)
 
-@router.websocket("/ws")
-async def websocket_rpc_endpoint(websocket: WebSocket, logged_in: bool = Depends(logged_in)):
+def init_pubsub_router(broadcaster_uri:str=BROADCAST_URI):
     """
-    this is the main websocket endpoint the sidecar uses to register on policy updates.
-    as you can see, this endpoint is protected by an HTTP Authorization Bearer token.
+    Args:
+        broadcaster_uri (str, optional): Which server/medium should the PubSub use for broadcasting. Defaults to BROADCAST_URI.
+        None means no broadcasting.
     """
-    if not logged_in:
-        logger.info("Closing connection", remote_address=websocket.client, reason="Authentication failed")
-        await websocket.close()
-        return
+    router = APIRouter()
+    endpoint = PubSubEndpoint(broadcaster=broadcaster_uri)
 
-    async with endpoint.broadcaster:
-        await endpoint.main_loop(websocket)
+    @router.websocket("/ws")
+    async def websocket_rpc_endpoint(websocket: WebSocket, logged_in: bool = Depends(logged_in)):
+        """
+        this is the main websocket endpoint the sidecar uses to register on policy updates.
+        as you can see, this endpoint is protected by an HTTP Authorization Bearer token.
+        """
+        if not logged_in:
+            logger.info("Closing connection", remote_address=websocket.client, reason="Authentication failed")
+            await websocket.close()
+            return
+        # Init PubSub main-loop with or without broadcasting
+        if broadcaster_uri is not None:
+            async with endpoint.broadcaster:
+                await endpoint.main_loop(websocket)
+        else:
+            await endpoint.main_loop(websocket)
