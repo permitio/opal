cc1eb9ec8cac42a8b8f317a7b554e3afe0bfe3bb
orweis <orweis@gmail.com>
2021-03-09 03:39:27 +0200

    init with components control, and docs

--- opal/client/opal_client.py
+++ opal/client/opal_client.py
@@ -1,3 +1,16 @@
+from logging import disable
+from opal.client.policy_store.base_policy_store_client import BasePolicyStoreClient
+from opal.client.enforcer.api import init_enforcer_api_router
+from opal.client.enforcer.runner import OpaRunner
+from opal.client.data.updater import DataUpdater
+from opal.client.policy.updater import PolicyUpdater
+from opal.client.server.middleware import configure_middleware
+from opal.client.local.api import init_local_cache_api_router
+from opal.client.data.api import router as data_router
+from opal.client.policy.api import init_policy_router
+from opal.client.server.api import router as proxy_router
+from opal.client.policy_store.policy_store_client_factory import PolicyStoreClientFactory
+from opal.client.config import OPENAPI_TAGS_METADATA, PolicyStoreTypes, POLICY_STORE_TYPE
 import asyncio
 from opal.client import policy_store
 from fastapi import FastAPI
@@ -5,47 +18,52 @@ from fastapi import FastAPI
 from fastapi_websocket_rpc.logger import logging_config, LoggingModes
 logging_config.set_mode(LoggingModes.UVICORN)
 
-from opal.client.config import OPENAPI_TAGS_METADATA, PolicyStoreTypes, POLICY_STORE_TYPE
-from opal.client.policy_store.policy_store_client_factory import PolicyStoreClientFactory
-from opal.client.server.api import router as proxy_router
-from opal.client.enforcer.api import init_enforcer_api_router 
-from opal.client.policy.api import init_policy_router
-from opal.client.data.api import router as data_router
-from opal.client.local.api import init_local_cache_api_router
-from opal.client.server.middleware import configure_middleware
-from opal.client.policy.updater import PolicyUpdater
-from opal.client.data.updater import DataUpdater
-from opal.client.enforcer.runner import OpaRunner
 
 
 class OpalClient:
+   
 
-    def __init__(self, policy_store_type=POLICY_STORE_TYPE) -> None:
+    def __init__(self,
+                 policy_store_type:PolicyStoreTypes=POLICY_STORE_TYPE,
+                 policy_store:BasePolicyStoreClient=None,
+                 data_updater:DataUpdater=None,
+                 policy_updater:PolicyUpdater=None
+                 ) -> None:
+        """[summary]
+
+        Args:
+            policy_store_type (PolicyStoreTypes, optional): [description]. Defaults to POLICY_STORE_TYPE.
+
+            Internal components (for each pass None for default init, or False to disable):
+                policy_store (BasePolicyStoreClient, optional): The policy store client. Defaults to None.
+                data_updater (DataUpdater, optional): Defaults to None.
+                policy_updater (PolicyUpdater, optional): Defaults to None.
+        """
         # Init policy store client
-        self.policy_store_type = policy_store_type
-        self.policy_store = PolicyStoreClientFactory.create(policy_store_type)
+        self.policy_store_type:PolicyStoreTypes = policy_store_type
+        self.policy_store:BasePolicyStoreClient = policy_store or PolicyStoreClientFactory.create(policy_store_type)
         # Init policy updater
-        self.policy_updater = PolicyUpdater()
+        self.policy_updater = policy_updater if policy_updater is not None else PolicyUpdater()
         # Data updating service
-        self.data_updater = DataUpdater(policy_store=policy_store)
+        self.data_updater = data_updater if data_updater is not None else DataUpdater(policy_store=policy_store)
 
         # Internal services
         # Policy store
         if self.policy_store_type == PolicyStoreTypes.OPA:
             self.opa_runner = OpaRunner.setup_opa_runner()
         else:
-            self.opa_runner = None
-            
+            self.opa_runner = False
+
         # init fastapi app
-        self.app:FastAPI = self._init_fast_api_app()
+        self.app: FastAPI = self._init_fast_api_app()
 
     def _init_fast_api_app(self):
         policy_store = self.policy_store
         app = FastAPI(
             title="OPAL client Sidecar",
-            description="This sidecar wraps Open Policy Agent (OPA) with a higher-level API intended for fine grained " + \
-                "application-level authorization. The sidecar automatically handles pulling policy updates in real-time " + \
-                "from a centrally managed cloud-service (api.authorizon.com).",
+            description="This sidecar wraps Open Policy Agent (OPA) with a higher-level API intended for fine grained " +
+            "application-level authorization. The sidecar automatically handles pulling policy updates in real-time " +
+            "from a centrally managed cloud-service (api.authorizon.com).",
             version="0.1.0",
             openapi_tags=OPENAPI_TAGS_METADATA
         )
@@ -71,20 +89,22 @@ class OpalClient:
 
         @app.on_event("startup")
         async def startup_event():
-            if self.opa_runner is not None:
+            if self.opa_runner:
                 self.opa_runner.start()
                 # wait for opa
-                await asyncio.sleep(1) 
-            self.policy_updater.start()
-            await self.data_updater.start()
+                await asyncio.sleep(1)
+            if self.policy_updater:
+                self.policy_updater.start()
+            if self.data_updater:
+                await self.data_updater.start()
 
         @app.on_event("shutdown")
         async def shutdown_event():
-            await self.data_updater.stop()
-            await self.policy_updater.stop()
-            if self.opa_runner is not None:
+            if self.data_updater:
+                await self.data_updater.stop()
+            if self.policy_updater:
+                await self.policy_updater.stop()
+            if self.opa_runner:
                 self.opa_runner.stop()
 
         return app
-
-
