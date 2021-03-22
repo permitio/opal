51455fdd450e58f20937e22a5bd9263f682599f4
Asaf Cohen <asafc@me.com>
2021-03-22 16:37:06 +0200

    opal server security env vars

--- opal/server/config.py
+++ opal/server/config.py
@@ -1,12 +1,27 @@
-from opal.common.schemas.data import DataSourceConfig
 from opal.common.confi import Confi
-
+from opal.common.schemas.data import DataSourceConfig
+from opal.common.authentication.keys import EncryptionKeyFormat, JWTAlgorithm
 
 confi = Confi()
 # ws server (TODO: merge with opal client config)
 OPAL_WS_LOCAL_URL = confi.str("OPAL_WS_LOCAL_URL", "ws://localhost:7002/ws")
 OPAL_WS_TOKEN = confi.str("OPAL_WS_TOKEN", "THIS_IS_A_DEV_SECRET")
 BROADCAST_URI = confi.str("BROADCAST_URI", "postgres://localhost/acalladb")
+
+# server security
+AUTH_PRIVATE_KEY_FORMAT = confi.enum("AUTH_PRIVATE_KEY_FORMAT", EncryptionKeyFormat, EncryptionKeyFormat.pem)
+AUTH_PRIVATE_KEY_PASSPHRASE = confi.str("AUTH_PRIVATE_KEY_PASSPHRASE", "")
+AUTH_PRIVATE_KEY = confi.str("AUTH_PRIVATE_KEY", None)
+
+AUTH_PUBLIC_KEY_FORMAT = confi.enum("AUTH_PUBLIC_KEY_FORMAT", EncryptionKeyFormat, EncryptionKeyFormat.ssh)
+AUTH_PUBLIC_KEY = confi.str("AUTH_PUBLIC_KEY", None)
+AUTH_JWT_ALGORITHM = confi.enum(
+    "AUTH_JWT_ALGORITHM",
+    JWTAlgorithm,
+    getattr(JWTAlgorithm, "RS256"),
+    description="jwt algorithm, possible values: see: https://pyjwt.readthedocs.io/en/stable/algorithms.html"
+)
+AUTH_MASTER_TOKEN = confi.str("AUTH_MASTER_TOKEN", None)
 
 # repo watcher
 POLICY_REPO_URL = confi.str("POLICY_REPO_URL", None)
