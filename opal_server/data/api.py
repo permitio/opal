eed348e045212dbbac98fc404f36df8ed286cdcf
Asaf Cohen <asafc@me.com>
2021-03-31 19:26:38 +0300

    fixed: default all-data route returns empty 200 response instead of 404

--- opal_server/data/api.py
+++ opal_server/data/api.py
@@ -2,12 +2,17 @@ from fastapi import APIRouter
 
 from opal_common.logger import logger
 from opal_common.schemas.data import DataUpdate
-from opal_server.config import DATA_CONFIG_ROUTE, DataSourceConfig
+from opal_server.config import DataSourceConfig, DATA_CONFIG_ROUTE, ALL_DATA_ROUTE
 from opal_server.data.data_update_publisher import DataUpdatePublisher
 
 
 def init_data_updates_router(data_update_publisher: DataUpdatePublisher, data_sources_config: DataSourceConfig):
     router = APIRouter()
+
+    @router.get(ALL_DATA_ROUTE)
+    async def default_all_data():
+        logger.warning("Serving default all-data route, meaning DATA_CONFIG_SOURCES was not configured!")
+        return {}
 
     @router.get(DATA_CONFIG_ROUTE)
     async def get_default_data_config():
