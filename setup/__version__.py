22c0ed8d513caaf0522b366c5689d3693fe0e8f5
Asaf Cohen <asafc@me.com>
2021-04-08 02:36:51 +0300

    bump version

--- setup/__version__.py
+++ setup/__version__.py
@@ -7,7 +7,7 @@ changes to your authorization policies and pushes live updates to your policy ag
 Project homepage: https://github.com/authorizon/opal
 """
 
-VERSION = (0, 0, 16)
+VERSION = (0, 0, 17)
 VERSION_STRING = '.'.join(map(str,VERSION))
 
 __version__ = VERSION_STRING
