498fc25146e905a4a4cac849fd6cbeb25767804d
orweis <orweis@gmail.com>
2021-04-25 15:30:54 +0300

    refactor url handling  -support same url more than once and  - support returning errors

--- opal_client/data/fetcher.py
+++ opal_client/data/fetcher.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Dict, Any
+from typing import Dict, Any, List, Tuple
 
 from opal_client.config import opal_client_config
 from opal_common.utils import get_authorization_header
@@ -53,49 +53,45 @@ class DataFetcher:
         """
         await self._engine.terminate_workers()
 
-    async def _handle_url(self, url, config):
+    async def handle_url(self, url, config):
         """
-        Helper function wrapping self._engine.handle_url, returning the fetched result with the url used for it
+        Helper function wrapping self._engine.handle_url
         """
         logger.info("Fetching data from url: {url}", url=url)
         try:
             # ask the engine to get our data
             response = await self._engine.handle_url(url, config=config)
-            # store as part of all results
-            return url, response
+            return response
         except asyncio.TimeoutError as e:
             logger.exception("Timeout while fetching url: {url}", url=url)
             raise
 
-    async def fetch_policy_data(self, urls: Dict[str, FetcherConfig] = None) -> Dict[str, Any]:
+    async def handle_urls(self, urls: List[Tuple[str, FetcherConfig]] = None) -> List[Tuple[str, FetcherConfig, Any]]:
         """
         Fetch data for each given url with the (optional) fetching configuration; return the resulting data mapped to each URL
 
         Args:
-            urls (Dict[str, FetcherConfig], optional): Urls (and fetching configuration) to fetch from.
+            urls (List[Tuple[str, FetcherConfig]], optional): Urls (and fetching configuration) to fetch from.
             Defaults to None - init data_url with HttpFetcherConfig (loaded with the provided auth token).
 
         Returns:
-            Dict[str, Any]: urls mapped to their resulting fetched data
+            List[Tuple[str,FetcherConfig, Any]]: urls mapped to their resulting fetched data
         """
-        # return value - /fetch-results mapped by url
-        results_by_url = {}
+        
+        
         # tasks
         tasks = []
         # if no url provided - default to the builtin route
         if urls is None:
-            urls = {self._data_url:  self._default_fetcher_config}
+            urls = [(self._data_url, self._default_fetcher_config)]
         # create a task for each url
-        for url, config in urls.items():
-            tasks.append(self._handle_url(url, config))
+        for url, config in urls:
+            tasks.append(self.handle_url(url, config))
         # wait for all data fetches to complete
         results = await asyncio.gather(*tasks, return_exceptions=True)
 
-        # Map results by urls
-        results_by_url = {}
-        for result in results:
-            if not isinstance(result, Exception):
-                url, response = result
-                results_by_url[url] = response
+        # Map results with their matching urls and config
+        results_with_url_and_config = [ (url, config, result)  for (url, config), result in zip(urls, results)]
+                
         # return results
-        return results_by_url
+        return results_with_url_and_config
