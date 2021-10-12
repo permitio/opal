d52484db127085a440febcd0e7b4301854030df9
Oded BD <oded@authorizon.com>
2021-10-12 18:38:49 +0300

    Create load conf util func

--- opal_server/policy/github_webhook/listener.py
+++ opal_server/policy/github_webhook/listener.py
@@ -9,6 +9,7 @@ publishes on the `webhook` topic, and the listener's callback is triggered.
 """
 
 from fastapi_websocket_pubsub.pub_sub_client import PubSubClient, Topic
+from opal_common.confi.utils import load_conf_if_none
 from opal_common.utils import get_authorization_header
 from opal_common.topics.listener import TopicListener, TopicCallback
 from opal_server.config import opal_server_config
@@ -21,8 +22,8 @@ def setup_webhook_listener(
     topic: Topic = "webhook",
 ) -> TopicListener:
     # load defaults
-    server_uri = server_uri or opal_server_config.OPAL_WS_LOCAL_URL
-    server_token = server_token or opal_server_config.OPAL_WS_TOKEN
+    server_uri = load_conf_if_none(server_uri, opal_server_config.OPAL_WS_LOCAL_URL)
+    server_token = load_conf_if_none(server_token, opal_server_config.OPAL_WS_TOKEN)
 
     return TopicListener(
         client=PubSubClient(
