0c87771319cd59f10fdad45e80c942ea8af3c877
Asaf Cohen <asafc@me.com>
2021-10-12 16:50:47 +0300

    integrate rookout

---
+++ scripts/gunicorn_conf.py
@@ -0,0 +1,25 @@
+import os
+
+from opal_common.logger import logger
+
+
+def post_fork(server, worker):
+    """
+    this hook takes effect if we are using gunicorn to run OPAL.
+    """
+    rookout_token = os.getenv("ROOKOUT_TOKEN", None)
+    if not rookout_token:
+        logger.info("No rookout token found, skipping.")
+        return
+
+    service = os.getenv("ROOKOUT_SERVICE", "opal_server")
+    env = os.getenv("ROOKOUT_ENV", "dev")
+    user = os.getenv("ROOKOUT_USER", None)
+
+    logger.info("Running Rookout...")
+    labels = {"env": env, "service": service}
+    if user is not None:
+        labels.update({"user": user})
+
+    import rook
+    rook.start(token=rookout_token, labels=labels)
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env sh
 set -e
 
+export GUNICORN_CONF=${GUNICORN_CONF:-/gunicorn_conf.py}
+
 # Start Gunicorn
-exec gunicorn -b 0.0.0.0:${UVICORN_PORT} -k uvicorn.workers.UvicornWorker --workers=${UVICORN_NUM_WORKERS} ${UVICORN_ASGI_APP}
+exec gunicorn -b 0.0.0.0:${UVICORN_PORT} -k uvicorn.workers.UvicornWorker --workers=${UVICORN_NUM_WORKERS} -c ${GUNICORN_CONF} ${UVICORN_ASGI_APP}
