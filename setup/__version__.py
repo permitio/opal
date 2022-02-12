6b0c61d0bd5d08fc5291827ff6af8587d6dbc775
Asaf Cohen <asafc@me.com>
2022-02-12 12:36:03 +0200

    bump version: 0.1.21

--- setup/__version__.py
+++ setup/__version__.py
@@ -7,7 +7,7 @@ changes to your authorization policies and pushes live updates to your policy ag
 Project homepage: https://github.com/permitio/opal
 """
 
-VERSION = (0, 1, 20)
+VERSION = (0, 1, 21)
 VERSION_STRING = '.'.join(map(str,VERSION))
 
 __version__ = VERSION_STRING
