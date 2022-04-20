ba77b86cecfb66a637dc56e9748c304fe1037485
Asaf Cohen <asafc@me.com>
2022-04-20 09:06:56 +0300

    fix webhook endpoint when git source is api bundle server

--- packages/opal-server/opal_server/policy/webhook/api.py
+++ packages/opal-server/opal_server/policy/webhook/api.py
@@ -9,14 +9,18 @@ from opal_server.policy.webhook.deps import validate_github_signature_or_throw, 
 
 
 def init_git_webhook_router(pubsub_endpoint: PubSubEndpoint, authenticator: JWTAuthenticator):
-    router = APIRouter()
+    async def dummy_affected_repo_urls(request: Request) -> List[str]:
+        return []
+
     source_type = opal_server_config.POLICY_SOURCE_TYPE
     if source_type == PolicySourceTypes.Api:
         route_dependency = authenticator
-        func_dependency = []
+        func_dependency = dummy_affected_repo_urls
     else:
         route_dependency = validate_github_signature_or_throw
         func_dependency = affected_repo_urls
+
+    router = APIRouter()
 
     @router.post(
         "/webhook",
@@ -27,22 +31,22 @@ def init_git_webhook_router(pubsub_endpoint: PubSubEndpoint, authenticator: JWTA
         request: Request,
         urls: List[str] = Depends(func_dependency)
     ):
-        event = request.headers.get('X-GitHub-Event', 'ping')
+        # TODO: breaking change: change "repo_url" to "remote_url" in next major
+        if source_type == PolicySourceTypes.Git:
+            event = request.headers.get('X-GitHub-Event', 'ping')
 
-        if (opal_server_config.POLICY_REPO_URL is not None and opal_server_config.POLICY_REPO_URL in urls)\
-                or \
-                (source_type == PolicySourceTypes.Api):
-            if source_type == PolicySourceTypes.Api:
-                logger.info("Triggered webhook to check API bundle URL")
-                response = {"status": "ok", "remote": opal_server_config.POLICY_BUNDLE_URL}
-            elif source_type == PolicySourceTypes.Git:
-                logger.info("triggered webhook on repo: {repo}", repo=urls[0], hook_event=event)
-                response = {"status": "ok", "event": event, "repo_url": urls[0]}
-            if event == 'push' or source_type == PolicySourceTypes.Api:
-                # notifies the webhook listener via the pubsub broadcaster
-                await pubsub_endpoint.publish(opal_server_config.POLICY_REPO_WEBHOOK_TOPIC)
-            return response
+            if opal_server_config.POLICY_REPO_URL is not None and opal_server_config.POLICY_REPO_URL in urls:
+                logger.info("triggered webhook on repo: {repo}", repo=opal_server_config.POLICY_REPO_URL, hook_event=event)
+                if event == 'push':
+                    # notifies the webhook listener via the pubsub broadcaster
+                    await pubsub_endpoint.publish(opal_server_config.POLICY_REPO_WEBHOOK_TOPIC)
+                return { "status": "ok", "event": event, "repo_url": opal_server_config.POLICY_REPO_URL }
+
+        elif source_type == PolicySourceTypes.Api:
+            logger.info("Triggered webhook to check API bundle URL")
+            await pubsub_endpoint.publish(opal_server_config.POLICY_REPO_WEBHOOK_TOPIC)
+            return {"status": "ok", "event": "webhook_trigger", "repo_url": opal_server_config.POLICY_BUNDLE_URL}
 
         return {"status": "ignored", "event": event}
-    return router
 
+    return router
