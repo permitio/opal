1f6c4340d8589cec0d7de2752d3242778ddb954a
Ori Shavit <ori@permit.io>
2022-06-15 15:06:40 +0300

    Clone git repositories via celery

--- packages/opal-server/opal_server/worker.py
+++ packages/opal-server/opal_server/worker.py
@@ -1,7 +1,10 @@
 import asyncio
+from pathlib import Path
 
 from celery import Celery
+from opal_common.schemas.policy_source import GitPolicySource
 from opal_server.config import opal_server_config
+from opal_server.git_fetcher import GitPolicyFetcher
 from opal_server.redis import RedisDB
 from opal_server.scopes.scope_repository import ScopeRepository
 
@@ -16,14 +19,27 @@ def async_to_sync(callable, *args, **kwargs):
 
 
 class Worker:
-    def __init__(self, scopes: ScopeRepository):
+    def __init__(self, base_dir: Path, scopes: ScopeRepository):
+        self._base_dir = base_dir
         self._scopes = scopes
 
     async def sync_scope(self, scope_id: str):
         scope = await self._scopes.get(scope_id)
 
+        fetcher = None
 
-worker = Worker(scopes=ScopeRepository(RedisDB(opal_server_config.REDIS_URL)))
+        if isinstance(scope.policy, GitPolicySource):
+            fetcher = GitPolicyFetcher(self._base_dir, scope)
+
+        if fetcher:
+            await fetcher.fetch()
+
+
+opal_base_dir = Path(opal_server_config.BASE_DIR)
+worker = Worker(
+    base_dir=opal_base_dir,
+    scopes=ScopeRepository(RedisDB(opal_server_config.REDIS_URL)),
+)
 app = Celery("opal-worker", broker=opal_server_config.REDIS_URL)
 
 
