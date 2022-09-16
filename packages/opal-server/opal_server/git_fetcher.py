a6f0c24d5d26637679b9f0ee0c9833f08810c1e7
Asaf Cohen <asaf@permit.io>
2022-09-16 13:53:08 +0300

    git policy fetcher: if repo was found, make sure it is valid and otherwise reclone

--- packages/opal-server/opal_server/git_fetcher.py
+++ packages/opal-server/opal_server/git_fetcher.py
@@ -1,8 +1,11 @@
+import shutil
 from pathlib import Path
 from typing import Optional, cast
 
 import aiofiles.os
+import pygit2
 from git import Repo
+from opal_common.async_utils import run_sync
 from opal_common.git.bundle_maker import BundleMaker
 from opal_common.logger import logger
 from opal_common.schemas.policy import PolicyBundle
@@ -35,6 +38,64 @@ class PolicyFetcher:
         pass
 
 
+class RepoInterface:
+    """Manages a git repo with pygit2."""
+
+    @staticmethod
+    def create_local_branch_ref(
+        repo: Repository,
+        branch_name: str,
+        remote_name: str,
+        base_branch: str,
+    ) -> pygit2.Reference:
+        if branch_name not in repo.branches.local:
+            remote_branch = f"{remote_name}/{branch_name}"
+            base_remote_branch = f"{remote_name}/{base_branch}"
+            if remote_branch in repo.branches.remote:
+                (commit, _) = repo.resolve_refish(remote_branch)
+            elif repo.branches.remote.get(base_remote_branch) is not None:
+                (commit, _) = repo.resolve_refish(base_remote_branch)
+            else:
+                raise RuntimeError(
+                    "Both branch and base branch were not found on remote"
+                )
+            logger.debug(
+                f"Created local branch '{branch_name}', pointing to: {commit.hex}"
+            )
+            return repo.create_reference(f"refs/heads/{branch_name}", commit.hex)
+        else:
+            logger.debug(
+                f"No need to create local branch '{branch_name}': already exists!"
+            )
+            return repo.references[f"refs/heads/{branch_name}"]
+
+    @staticmethod
+    def checkout_local_branch_from_remote(
+        repo: Repository,
+        branch_name: str,
+        remote_name: str,
+    ):
+        ref = RepoInterface.create_local_branch_ref(repo, branch_name, remote_name)
+        repo.checkout(ref)
+
+    @staticmethod
+    def verify_found_repo_matches_origin(
+        origin_url: str, clone_path: str
+    ) -> Repository:
+        """verifies that the repo we found in the directory matches the repo we
+        are wishing to clone."""
+        repo = Repository(clone_path)
+        for remote in repo.remotes:
+            if remote.url == origin_url:
+                logger.debug(
+                    f"found target repo url is referred by remote: {remote.name}, url={remote.url}"
+                )
+                return
+        error: str = f"Repo mismatch! No remote matches target url: {origin_url}, found urls: {[remote.url for remote in repo.remotes]}"
+        logger.error(error)
+        raise ValueError(error)
+
+
 class GitPolicyFetcher(PolicyFetcher):
     def __init__(
         self,
@@ -52,32 +113,48 @@ class GitPolicyFetcher(PolicyFetcher):
     async def fetch(self):
         await aiofiles.os.makedirs(str(self._base_dir), exist_ok=True)
 
-        if discover_repository(str(self._repo_path)):
-            await self._pull()
-        else:
-            await self._clone()
+        if self._discover_repository(self._repo_path):
+            logger.debug(f"repo found at {self._repo_path}")
+            repo = self._get_valid_repo_at(str(self._repo_path))
+            if repo is not None:
+                return await self._pull(repo)
+            else:
+                # repo dir exists but invalid -> we must delete the directory
+                shutil.rmtree(self._repo_path)
+
+        # fallthrough to clean clone
+        return await self._clone()
+
+    def _discover_repository(self, path: Path) -> bool:
+        git_path: Path = path / ".git"
+        return discover_repository(str(path)) and git_path.exists()
 
     async def _clone(self):
-        logger.info("Cloning new scope to {path}", path=self._repo_path)
-        clone_repository(
+        logger.info("Cloning scope to {path}", path=self._repo_path)
+        repo: Repository = await run_sync(
+            clone_repository,
             self._source.url,
             str(self._repo_path),
             callbacks=self._auth_callbacks,
             checkout_branch=self._source.branch,
         )
-
-        repo = Repository(self._repo_path)
         await self.callbacks.on_update(None, repo.head.target.hex)
 
-    async def _pull(self):
-        logger.info("Checking for new commits in {path}", path=self._repo_path)
-        repo = Repository(self._repo_path)
+    def _get_valid_repo_at(self, path: str) -> Optional[Repository]:
+        RepoInterface.verify_found_repo_matches_origin(self._source.url, path)
+        logger.info("Checking for new commits in {path}", path=path)
+        try:
+            return Repository(path)
+        except pygit2.GitError:
+            logger.error("invalid repo at: {path}", path=path)
+            return None
+
+    async def _pull(self, repo: Repository):
         old_revision = repo.head.target.hex
-        repo.remotes["origin"].fetch(callbacks=self._auth_callbacks)
+        await run_sync(repo.remotes["origin"].fetch, callbacks=self._auth_callbacks)
         repo.checkout(
             repo.references[f"refs/remotes/origin/{self._source.branch}"].resolve().name
         )
-
         await self.callbacks.on_update(old_revision, repo.head.target.hex)
 
     def make_bundle(self, base_hash: Optional[str] = None) -> PolicyBundle:
