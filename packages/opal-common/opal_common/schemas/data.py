d92cc125c1606c923ef5333a07a11e26d2a346ba
Ori Shavit <ori@permit.io>
2023-01-30 15:27:11 +0200

    Return policy for default scope when unavailable for requested scope
    On two scenarios, there is no valid policy to return:
    1. When the scope requested does not (yet) exist
    2. When the scope exist, but the repository was
       not cloned properly (also, yet).
    
    In both cases, the API will only answer requests that have the
    requested scope ID in the JWT token claims, so requests
    for scopes that should not exist, will be denied.
    
    Under these conditions, OPAL Server will return the policy
    for the `default` scope, which is set by the OPAL_POLICY_REPO_*
    config family.

--- packages/opal-common/opal_common/schemas/data.py
+++ packages/opal-common/opal_common/schemas/data.py
@@ -57,7 +57,7 @@ class DataSourceConfig(BaseModel):
     """
 
     entries: List[DataSourceEntryWithPollingInterval] = Field(
-        ..., description="list of data sources and how to fetch from them"
+        [], description="list of data sources and how to fetch from them"
     )
 
 
