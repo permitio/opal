8fb587940338c90833c275ade4d87af00ccae5b2
Ro'e Katz <roekatz@gmail.com>
2023-03-29 15:52:10 +0300

    GitFetcher: Protect fetch with a file lock
    To prevent concurrent execution in the new task based mechanism

--- packages/opal-server/opal_server/git_fetcher.py
+++ packages/opal-server/opal_server/git_fetcher.py
@@ -17,6 +17,7 @@ from opal_common.schemas.policy_source import (
     GitPolicyScopeSource,
     SSHAuthData,
 )
+from opal_common.synchronization.named_lock import NamedLock
 from pygit2 import (
     KeypairFromMemory,
     RemoteCallbacks,
@@ -142,6 +143,14 @@ class GitPolicyFetcher(PolicyFetcher):
             f"Initializing git fetcher: scope_id={scope_id}, url={source.url}, branch={self._source.branch}, path={GitPolicyFetcher.source_id(source)}"
         )
 
+    async def _get_repo_lock(self):
+        locks_dir = self._base_dir / ".locks"
+        await aiofiles.os.makedirs(str(locks_dir), exist_ok=True)
+
+        return NamedLock(
+            locks_dir / GitPolicyFetcher.source_id(self._source), attempt_interval=0.1
+        )
+
     async def fetch_and_notify_on_changes(
         self, hinted_hash: Optional[str] = None, force_fetch: bool = False
     ):
@@ -153,36 +162,39 @@ class GitPolicyFetcher(PolicyFetcher):
         - if the hinted commit hash is provided and is already found in the local clone
         we use this hint to avoid an necessary fetch.
         """
-        await aiofiles.os.makedirs(str(self._base_dir), exist_ok=True)
-
-        if self._discover_repository(self._repo_path):
-            logger.info("Repo found at {path}", path=self._repo_path)
-            repo = self._get_valid_repo()
-            if repo is not None:
-                should_fetch = await self._should_fetch(
-                    repo, hinted_hash=hinted_hash, force_fetch=force_fetch
-                )
-                if should_fetch:
-                    logger.info(
-                        f"Fetching remote (force_fetch={force_fetch}): {self._remote} ({self._source.url})"
+        repo_lock = await self._get_repo_lock()
+        async with repo_lock:
+            if self._discover_repository(self._repo_path):
+                logger.info("Repo found at {path}", path=self._repo_path)
+                repo = self._get_valid_repo()
+                if repo is not None:
+                    should_fetch = await self._should_fetch(
+                        repo, hinted_hash=hinted_hash, force_fetch=force_fetch
                     )
-                    await run_sync(
-                        repo.remotes[self._remote].fetch, callbacks=self._auth_callbacks
-                    )
-                    logger.debug(f"Fetch completed: {self._source.url}")
+                    if should_fetch:
+                        logger.info(
+                            f"Fetching remote (force_fetch={force_fetch}): {self._remote} ({self._source.url})"
+                        )
+                        await run_sync(
+                            repo.remotes[self._remote].fetch,
+                            callbacks=self._auth_callbacks,
+                        )
+                        logger.debug(f"Fetch completed: {self._source.url}")
 
-                # New commits might be present because of a previous fetch made by another scope
-                await self._notify_on_changes(repo)
-                return
+                    # New commits might be present because of a previous fetch made by another scope
+                    await self._notify_on_changes(repo)
+                    return
+                else:
+                    # repo dir exists but invalid -> we must delete the directory
+                    logger.warning(
+                        "Deleting invalid repo: {path}", path=self._repo_path
+                    )
+                    shutil.rmtree(self._repo_path)
             else:
-                # repo dir exists but invalid -> we must delete the directory
-                logger.warning("Deleting invalid repo: {path}", path=self._repo_path)
-                shutil.rmtree(self._repo_path)
-        else:
-            logger.info("Repo not found at {path}", path=self._repo_path)
+                logger.info("Repo not found at {path}", path=self._repo_path)
 
-        # fallthrough to clean clone
-        await self._clone()
+            # fallthrough to clean clone
+            await self._clone()
 
     def _discover_repository(self, path: Path) -> bool:
         git_path: Path = path / ".git"
