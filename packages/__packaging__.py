f816c8a01a61df68ecfd91596bd7f7135698bc8c
Ro'e Katz <roekatz@gmail.com>
2023-05-01 13:46:33 +0300

    Bump OPAL pkg version to 0.6.1

--- packages/__packaging__.py
+++ packages/__packaging__.py
@@ -8,7 +8,7 @@ Project homepage: https://github.com/permitio/opal
 """
 import os
 
-VERSION = (0, 6, 0)
+VERSION = (0, 6, 1)
 VERSION_STRING = ".".join(map(str, VERSION))
 
 __version__ = VERSION_STRING
