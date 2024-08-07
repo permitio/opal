56d9915f4a45b770e072eafdc8deeab01681b5db
eli <eli@permit.io>
2024-08-07 08:47:55 -0700

    wip2

--- packages/__packaging__.py
+++ packages/__packaging__.py
@@ -8,12 +8,8 @@ Project homepage: https://github.com/permitio/opal
 """
 
 import os
-
-VERSION = (
-    0,
-    0,
-    0,
-)  # It's a placeholder, the real version is set by the release CI/CD pipeline
+# VERSION is a placeholder, the real version is set by the release CI/CD pipeline
+VERSION = (0, 0, 0)
 VERSION_STRING = ".".join(map(str, VERSION))
 
 __version__ = VERSION_STRING
