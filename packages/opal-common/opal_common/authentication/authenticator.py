55ced17ea519ee74b62503305bb529fc683067c1
Ondrej Scecina <scecina_ondrej@solarturbines.com>
2024-08-16 08:39:10 +0200

    Enable OAuth2 authentication.
    # Conflicts:
    #	packages/opal-client/opal_client/client.py
    #	packages/opal-common/opal_common/authentication/authenticator.py
    #	packages/opal-common/opal_common/authentication/jwk.py
    #	packages/opal-common/opal_common/authentication/oauth2.py
    #	packages/opal-common/opal_common/config.py
    #	packages/opal-server/opal_server/authentication/authenticator.py
    #	packages/opal-server/opal_server/data/api.py
    #	packages/opal-server/opal_server/server.py
    #	packages/requires.txt

--- packages/opal-common/opal_common/authentication/authenticator.py
+++ packages/opal-common/opal_common/authentication/authenticator.py
@@ -2,14 +2,12 @@ from abc import abstractmethod
 from typing import Optional
 
 from fastapi import Header
+from opal_common.config import opal_common_config
 from opal_common.authentication.deps import JWTAuthenticator
 from opal_common.authentication.types import JWTClaims
 from opal_common.authentication.verifier import JWTVerifier, Unauthorized
-from opal_common.config import opal_common_config
 from opal_common.logger import logger
-
 from .oauth2 import CachedOAuth2Authenticator, OAuth2ClientCredentialsAuthenticator
-
 
 class Authenticator:
     @property
@@ -17,25 +15,18 @@ class Authenticator:
         return self._delegate().enabled
 
     async def authenticate(self, headers):
-        if hasattr(self._delegate(), "authenticate") and callable(
-            getattr(self._delegate(), "authenticate")
-        ):
+        if hasattr(self._delegate(), "authenticate") and callable(getattr(self._delegate(), "authenticate")):
             await self._delegate().authenticate(headers)
 
     @abstractmethod
     def _delegate(self) -> dict:
         pass
 
-
 class _ClientAuthenticator(Authenticator):
     def __init__(self):
         if opal_common_config.AUTH_TYPE == "oauth2":
-            self.__delegate = CachedOAuth2Authenticator(
-                OAuth2ClientCredentialsAuthenticator()
-            )
-            logger.info(
-                "OPAL is running in secure mode - will authenticate API requests."
-            )
+            self.__delegate = CachedOAuth2Authenticator(OAuth2ClientCredentialsAuthenticator())
+            logger.info("OPAL is running in secure mode - will authenticate API requests.")
         else:
             self.__delegate = JWTAuthenticator(self.__verifier())
 
@@ -47,15 +38,12 @@ class _ClientAuthenticator(Authenticator):
             issuer=opal_common_config.AUTH_JWT_ISSUER,
         )
         if not verifier.enabled:
-            logger.info(
-                "API authentication disabled (public encryption key was not provided)"
-            )
+            logger.info("API authentication disabled (public encryption key was not provided)")
 
         return verifier
 
     def _delegate(self) -> dict:
         return self.__delegate
-
 
 class ClientAuthenticator(_ClientAuthenticator):
     def __call__(self, authorization: Optional[str] = Header(None)) -> JWTClaims:
