1c6b3ea594ada73787ac707d4f56c9ebc7f31737
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-10-11 16:46:00 +0530

    use exisitng docker-compose file

--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -6,12 +6,12 @@ import os
 @pytest.fixture(scope="session", autouse=True)
 def docker_compose_setup():
     """Spin up Docker containers for OPAL services using docker-compose."""
-    compose_file = os.path.abspath("./docker-compose.yml")
+    compose_file = os.path.abspath("../app-tests/docker-compose-app-tests.yml")
 
     subprocess.run(["docker-compose", "-f", compose_file, "up", "-d"])
     
     # Wait for services to be up and running
-    time.sleep(60)
+    time.sleep(10)
     
     yield
     
--- e2e_tests/tests/test_e2e.py
+++ e2e_tests/tests/test_e2e.py
@@ -1,14 +1,24 @@
 import requests
 import time
+import subprocess
+
+def check_logs(container_name):
+    result = subprocess.run(["docker", "logs", container_name], capture_output=True, text=True)
+    assert "ERROR" not in result.stdout and "CRITICAL" not in result.stdout, f"Critical errors found in {container_name}"
 
 def test_opal_server_health():
     """Test OPAL Server health endpoint."""
-    response = requests.get("http://localhost:7002/healthcheck")
+    response = requests.get("http://opal_server:7002/healthcheck")
     assert response.status_code == 200
 
 def test_opal_client_health():
     """Test OPAL Client endpoint."""
-
-    response = requests.get("http://localhost:7000/healthcheck")
+    response = requests.get("http://opal_client:7000/healthcheck")
     assert response.status_code == 200
     print(response.json())    
+
+def test_opal_server_logs():
+    check_logs("app-tests-opal_server-1")
+
+def test_opal_client_logs():
+    check_logs("app-tests-opal_client-1")
