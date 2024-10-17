81294de52af94a9cc2c1eea22e2211c73d64516c
Hans B. K. Tognon <git@tobihans.space>
2024-10-17 11:17:19 +0100

    wip: interconnect opal client & server

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,39 +1,49 @@
-import json
+from secrets import token_hex
+import time
 import pytest
+from testcontainers.core.generic import DockerContainer
+from testcontainers.core.waiting_utils import wait_for_logs
+from testcontainers.postgres import PostgresContainer
 
 from tests.containers import OpalClientContainer, OpalServerContainer
-from .settings import *  # noqa: F403
-from testcontainers.core.waiting_utils import wait_for_logs
-from testcontainers.core.generic import DockerContainer
-from testcontainers.postgres import PostgresContainer
 
 # https://stackoverflow.com/questions/7119452/git-commit-from-python
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def broadcast_channel():
-    with PostgresContainer("postgres:14.1-alpine", driver=None) as postgres:
-        yield postgres
+    with PostgresContainer("postgres:alpine", driver=None).with_name(
+        f"pytest_{token_hex(2)}_broadcast_channel"
+    ) as container:
+        yield container
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="session")
 def opal_server(broadcast_channel: PostgresContainer):
-    container = (
-        OpalServerContainer("permitio/opal-server")
-        .with_env("OPAL_BROADCAST_URI", broadcast_channel.get_connection_url())
-        .start()
-    )
-    wait_for_logs(container, "Clone succeeded")
-    yield container
-    container.stop()
+    opal_broadcast_uri = broadcast_channel.get_connection_url()
+
+    with OpalServerContainer("permitio/opal-server").with_env(
+        "OPAL_BROADCAST_URI", opal_broadcast_uri
+    ) as container:
+        wait_for_logs(container, "Clone succeeded")
+        yield container
 
 
-@pytest.fixture
+@pytest.fixture(scope="session", autouse=True)
 def opal_client(opal_server: OpalServerContainer):
-    container = (
-        OpalClientContainer("permitio/opal-client")
-        .with_env("OPAL_SERVER_URL", "")
-        .start()
+    opal_server_url = (
+        f"http://{get_container_ip(opal_server)}:{opal_server.get_exposed_port(7002)}"
     )
-    yield container
-    container.stop()
+
+    with OpalClientContainer().with_env(
+        "OPAL_SERVER_URL", opal_server_url
+    ) as container:
+        wait_for_logs(container, "")
+        yield container
+        time.sleep(300)
+
+
+def get_container_ip(container: DockerContainer):
+    _container = container.get_wrapped_container()
+    _container.reload()
+    return _container.attrs.get("NetworkSettings", {}).get("IPAddress")
