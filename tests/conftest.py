b2960866dd2f9bb0ecfaee7db116a294d3cb4f06
Hans B. K. Tognon <git@tobihans.space>
2024-10-19 15:41:46 +0100

    chore: savepoint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,63 +1,72 @@
 import time
-
 import docker
 import pytest
-from testcontainers.core.generic import DockerContainer
 from testcontainers.core.waiting_utils import wait_for_logs
 from testcontainers.postgres import PostgresContainer
 
-from tests.containers import OpalClientContainer, OpalServerContainer
+from tests.containers import OpalServerContainer
 
 from . import settings as s
 
 # https://stackoverflow.com/questions/7119452/git-commit-from-python
 
 
+@pytest.fixture(scope="session", autouse=True)
+def dump_env():
+    if s.OPAL_TESTS_DEBUG:
+        # Dump current config in a .env file for debugging purposes.
+        env = []
+        for key, val in vars(s).items():
+            if key.startswith("OPAL_"):
+                env.append(f"export {key}='{val}'\n\n")
+        with open(f"pytest_{s.OPAL_TESTS_UNIQ_ID}.env", "w") as envfile:
+            envfile.writelines(
+                [
+                    "#!/usr/bin/env bash\n\n",
+                    *env,
+                ]
+            )
+
+
 @pytest.fixture(scope="session")
 def opal_network():
     client = docker.from_env()
-    network = client.networks.create(
-        f"pytest_opal_{s.OPAL_TESTS_UNIQ_ID}", driver="bridge"
-    )
+    network = client.networks.create(s.OPAL_TESTS_NETWORK_NAME, driver="bridge")
     yield network.name
     network.remove()
 
 
 @pytest.fixture(scope="session")
 def broadcast_channel(opal_network: str):
-    with PostgresContainer(
-        "postgres:alpine", driver=None, network=opal_network
-    ).with_name(f"pytest_{s.OPAL_TESTS_UNIQ_ID}_broadcast_channel") as container:
-        # opal_network.connect(container.get_wrapped_container())
-        yield container
-
-
-@pytest.fixture(scope="session")
-def opal_server(opal_network: str, broadcast_channel: PostgresContainer):
-    opal_broadcast_uri = broadcast_channel.get_connection_url()
-
-    with OpalServerContainer(network=opal_network).with_env(
-        "OPAL_BROADCAST_URI", opal_broadcast_uri
+    with PostgresContainer("postgres:alpine", network=opal_network).with_name(
+        f"pytest_opal_brodcast_channel_{s.OPAL_TESTS_UNIQ_ID}"
     ) as container:
-        # opal_network.connect(container.get_wrapped_container())
-        wait_for_logs(container, "Clone succeeded")
         yield container
 
 
 @pytest.fixture(scope="session", autouse=True)
-def opal_client(opal_network: str, opal_server: OpalServerContainer):
-    opal_server_url = f"http://{opal_server._name}.{opal_network}:7002"
-    print(f"{opal_server_url=}")
+# @pytest.fixture(scope="session")
+def opal_server(opal_network: str, broadcast_channel: PostgresContainer):
+    opal_broadcast_uri = broadcast_channel.get_connection_url(
+        host=f"{broadcast_channel._name}.{opal_network}", driver=None
+    )
 
-    with OpalClientContainer(network=opal_network).with_env(
-        "OPAL_SERVER_URL", opal_server_url
+    with OpalServerContainer(network=opal_network).with_env(
+        "OPAL_BROADCAST_URI", opal_broadcast_uri
     ) as container:
-        wait_for_logs(container, "")
+        wait_for_logs(container, "Clone succeeded")
         yield container
-        time.sleep(300)
+        time.sleep(600)
 
 
-def get_container_ip(container: DockerContainer):
-    _container = container.get_wrapped_container()
-    _container.reload()
-    return _container.attrs.get("NetworkSettings", {}).get("IPAddress")
+# @pytest.fixture(scope="session", autouse=True)
+# def opal_client(opal_network: str, opal_server: OpalServerContainer):
+#     opal_server_url = f"http://{opal_server._name}.{opal_network}:7002"
+#
+#     with OpalClientContainer(network=opal_network).with_env(
+#         "OPAL_SERVER_URL", opal_server_url
+#     ) as container:
+#         wait_for_logs(container, "")
+#         yield container
+#         time.sleep(600)
+#
--- tests/containers.py
+++ tests/containers.py
@@ -1,5 +1,3 @@
-import json
-
 from testcontainers.core.generic import DockerContainer
 
 from . import settings as s
@@ -12,32 +10,46 @@ class OpalServerContainer(DockerContainer):
         docker_client_kw: dict | None = None,
         **kwargs,
     ) -> None:
+        kwargs.update({"tty": s.OPAL_TESTS_DEBUG})
         super().__init__(image, docker_client_kw, **kwargs)
 
-        self.with_name(f"pytest_{s.OPAL_TESTS_UNIQ_ID}_opal_server")
+        self.with_name(s.OPAL_TESTS_SERVER_CONTAINER_NAME)
         self.with_exposed_ports(7002)
+
+        if s.OPAL_TESTS_DEBUG:
+            self.with_env("LOG_DIAGNOSE", "true")
+
         self.with_env("UVICORN_NUM_WORKERS", s.UVICORN_NUM_WORKERS)
+
         self.with_env("OPAL_POLICY_REPO_URL", s.OPAL_POLICY_REPO_URL)
-        self.with_env("OPAL_POLICY_REPO_SSH_KEY", s.OPAL_POLICY_REPO_SSH_KEY)
-        self.with_env("OPAL_POLICY_REPO_MAIN_BRANCH", s.OPAL_POLICY_REPO_MAIN_BRANCH)
         self.with_env(
             "OPAL_POLICY_REPO_POLLING_INTERVAL", s.OPAL_POLICY_REPO_POLLING_INTERVAL
         )
-        self.with_env("PAL_DATA_CONFIG_SOURCES", json.dumps(s.OPAL_DATA_CONFIG_SOURCES))
-        self.with_env("OPAL_LOG_FORMAT_INCLUDE_PID", s.OPAL_LOG_FORMAT_INCLUDE_PID)
+        self.with_env("OPAL_POLICY_REPO_MAIN_BRANCH", s.OPAL_POLICY_REPO_MAIN_BRANCH)
+        if s.OPAL_POLICY_REPO_SSH_KEY:
+            self.with_env("OPAL_POLICY_REPO_SSH_KEY", s.OPAL_POLICY_REPO_SSH_KEY)
         self.with_env(
             "OPAL_POLICY_REPO_WEBHOOK_SECRET", s.OPAL_POLICY_REPO_WEBHOOK_SECRET
         )
         self.with_env(
             "OPAL_POLICY_REPO_WEBHOOK_PARAMS", s.OPAL_POLICY_REPO_WEBHOOK_PARAMS
         )
+
+        self.with_env("OPAL_DATA_CONFIG_SOURCES", s.OPAL_DATA_CONFIG_SOURCES)
+        self.with_env("OPAL_LOG_FORMAT_INCLUDE_PID", s.OPAL_LOG_FORMAT_INCLUDE_PID)
+
+        self.with_env("OPAL_AUTH_MASTER_TOKEN", s.OPAL_AUTH_MASTER_TOKEN)
+
         self.with_env("OPAL_AUTH_PUBLIC_KEY", s.OPAL_AUTH_PUBLIC_KEY)
         self.with_env("OPAL_AUTH_PRIVATE_KEY", s.OPAL_AUTH_PRIVATE_KEY)
-        self.with_env("OPAL_AUTH_MASTER_TOKEN", s.OPAL_AUTH_MASTER_TOKEN)
+        if s.OPAL_AUTH_PRIVATE_KEY_PASSPHRASE:
+            self.with_env(
+                "OPAL_AUTH_PRIVATE_KEY_PASSPHRASE", s.OPAL_AUTH_PRIVATE_KEY_PASSPHRASE
+            )
         self.with_env("OPAL_AUTH_JWT_AUDIENCE", s.OPAL_AUTH_JWT_AUDIENCE)
         self.with_env("OPAL_AUTH_JWT_ISSUER", s.OPAL_AUTH_JWT_ISSUER)
         # FIXME: The env below is triggerring: did not find main branch: main,...
-        # self.with_env("OPAL_STATISTICS_ENABLED", s.OPAL_STATISTICS_ENABLED)
+        self.with_env("OPAL_STATISTICS_ENABLED", s.OPAL_STATISTICS_ENABLED)
 
 
 class OpalClientContainer(DockerContainer):
@@ -47,23 +59,24 @@ class OpalClientContainer(DockerContainer):
         docker_client_kw: dict | None = None,
         **kwargs,
     ) -> None:
+        kwargs.update({"tty": s.OPAL_TESTS_DEBUG})
         super().__init__(image, docker_client_kw, **kwargs)
 
-        self.with_name(f"pytest_{s.OPAL_TESTS_UNIQ_ID}_opal_client")  # noqa: F821
+        self.with_name(s.OPAL_TESTS_CLIENT_CONTAINER_NAME)
         self.with_exposed_ports(7000, 8181)
         self.with_env("OPAL_LOG_FORMAT_INCLUDE_PID", s.OPAL_LOG_FORMAT_INCLUDE_PID)
         self.with_env("OPAL_INLINE_OPA_LOG_FORMAT", s.OPAL_INLINE_OPA_LOG_FORMAT)
-        self.with_env(
-            "OPAL_SHOULD_REPORT_ON_DATA_UPDATES", s.OPAL_SHOULD_REPORT_ON_DATA_UPDATES
-        )
-        self.with_env("OPAL_DEFAULT_UPDATE_CALLBACKS", s.OPAL_DEFAULT_UPDATE_CALLBACKS)
-        self.with_env(
-            "OPAL_OPA_HEALTH_CHECK_POLICY_ENABLED",
-            s.OPAL_OPA_HEALTH_CHECK_POLICY_ENABLED,
-        )
-        self.with_env("OPAL_AUTH_PUBLIC_KEY", s.OPAL_AUTH_PUBLIC_KEY)
-        self.with_env("OPAL_CLIENT_TOKEN", s.OPAL_CLIENT_TOKEN)
-        self.with_env("OPAL_DATA_SOURCE_TOKEN", s.OPAL_DATA_SOURCE_TOKEN)
-        self.with_env("OPAL_AUTH_JWT_AUDIENCE", s.OPAL_AUTH_JWT_AUDIENCE)
-        self.with_env("OPAL_AUTH_JWT_ISSUER", s.OPAL_AUTH_JWT_ISSUER)
+        # self.with_env(
+        #     "OPAL_SHOULD_REPORT_ON_DATA_UPDATES", s.OPAL_SHOULD_REPORT_ON_DATA_UPDATES
+        # )
+        # self.with_env("OPAL_DEFAULT_UPDATE_CALLBACKS", s.OPAL_DEFAULT_UPDATE_CALLBACKS)
+        # self.with_env(
+        #     "OPAL_OPA_HEALTH_CHECK_POLICY_ENABLED",
+        #     s.OPAL_OPA_HEALTH_CHECK_POLICY_ENABLED,
+        # )
+        # self.with_env("OPAL_CLIENT_TOKEN", s.OPAL_CLIENT_TOKEN)
+        # self.with_env("OPAL_AUTH_JWT_AUDIENCE", s.OPAL_AUTH_JWT_AUDIENCE)
+        # self.with_env("OPAL_AUTH_JWT_ISSUER", s.OPAL_AUTH_JWT_ISSUER)
+        # self.with_env("OPAL_AUTH_PUBLIC_KEY", s.OPAL_AUTH_PUBLIC_KEY)
+        # self.with_env("OPAL_DATA_SOURCE_TOKEN", s.OPAL_DATA_SOURCE_TOKEN)
         # self.with_env("OPAL_STATISTICS_ENABLED", s.OPAL_STATISTICS_ENABLED)
