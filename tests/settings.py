6f96bdec8065d28df3007d2be0c3c8ccb9fc3c2c
Hans B. K. Tognon <git@tobihans.space>
2024-10-19 15:41:46 +0100

    chore: savepoint

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,6 @@
 import io
-from contextlib import redirect_stdout
 import json
+from contextlib import redirect_stdout
 from os import getenv as _
 from secrets import token_hex
 
@@ -9,12 +9,18 @@ from opal_common.schemas.security import PeerType
 from testcontainers.core.generic import DockerContainer
 from testcontainers.core.waiting_utils import wait_for_logs
 
-OPAL_TESTS_UNIQ_ID = token_hex(4)
+OPAL_TESTS_DEBUG = _("OPAL_TESTS_DEBUG") is not None
+
+OPAL_TESTS_UNIQ_ID = token_hex(2)
+OPAL_TESTS_NETWORK_NAME = f"pytest_opal_{OPAL_TESTS_UNIQ_ID}"
+OPAL_TESTS_SERVER_CONTAINER_NAME = f"pytest_opal_server_{OPAL_TESTS_UNIQ_ID}"
+OPAL_TESTS_CLIENT_CONTAINER_NAME = f"pytest_opal_client_{OPAL_TESTS_UNIQ_ID}"
 
 OPAL_IMAGE_TAG = _("OPAL_IMAGE_TAG", "latest")
 
 OPAL_AUTH_PUBLIC_KEY = _("OPAL_AUTH_PUBLIC_KEY", "")
 OPAL_AUTH_PRIVATE_KEY = _("OPAL_AUTH_PRIVATE_KEY", "")
+OPAL_AUTH_PRIVATE_KEY_PASSPHRASE = _("OPAL_AUTH_PRIVATE_KEY_PASSPHRASE", "")
 OPAL_AUTH_MASTER_TOKEN = _("OPAL_AUTH_MASTER_TOKEN", token_hex(16))
 OPAL_AUTH_JWT_AUDIENCE = _("OPAL_AUTH_JWT_AUDIENCE", "https://api.opal.ac/v1/")
 OPAL_AUTH_JWT_ISSUER = _("OPAL_AUTH_JWT_ISSUER", "https://opal.ac/")
@@ -52,6 +58,7 @@ with _container:
 
 UVICORN_NUM_WORKERS = _("UVICORN_NUM_WORKERS", "4")
 OPAL_STATISTICS_ENABLED = _("OPAL_STATISTICS_ENABLED", "true")
+
 OPAL_POLICY_REPO_URL = _(
     "OPAL_POLICY_REPO_URL", "git@github.com:permitio/opal-tests-policy-repo.git"
 )
@@ -73,13 +80,13 @@ OPAL_POLICY_REPO_WEBHOOK_PARAMS = _(
     ),
 )
 
+_url = f"http://{OPAL_TESTS_SERVER_CONTAINER_NAME}.{OPAL_TESTS_NETWORK_NAME}:7002/policy-data"
 OPAL_DATA_CONFIG_SOURCES = json.dumps(
     {
         "config": {
             "entries": [
                 {
-                    # TODO: Replace this
-                    "url": "http://localhost:7002/policy-data",
+                    "url": _url,
                     "config": {
                         "headers": {"Authorization": f"Bearer {OPAL_CLIENT_TOKEN}"}
                     },
@@ -99,7 +106,7 @@ OPAL_DEFAULT_UPDATE_CALLBACKS = json.dumps(
     {
         "callbacks": [
             [
-                "http://localhost:7002/data/callback_report",
+                f"http://{OPAL_TESTS_SERVER_CONTAINER_NAME}.{OPAL_TESTS_NETWORK_NAME}:7002/data/callback_report",
                 {
                     "method": "post",
                     "process_data": False,
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,4 +3,4 @@ def func(x):
 
 
 def test_answer():
-    assert func(3) == 5
+    assert func(4) == 5
